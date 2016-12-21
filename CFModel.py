817bd8312f42a99b4deef4947f988a3cd3071e49
Bradley P. Allen <bradley.p.allen@gmail.com>
2016-12-21 18:16:33 +0000

    Added DeepCFModel to CFModel.py

--- CFModel.py
+++ CFModel.py
@@ -9,7 +9,7 @@
 #
 
 import numpy as np
-from keras.layers import Embedding, Reshape, Merge
+from keras.layers import Embedding, Reshape, Merge, Dropout, Dense
 from keras.models import Sequential
 
 class CFModel(Sequential):
@@ -23,6 +23,25 @@ class CFModel(Sequential):
         Q.add(Reshape((k_factors,)))
         super(CFModel, self).__init__(**kwargs)
         self.add(Merge([P, Q], mode='dot', dot_axes=1))
+
+    def rate(self, user_id, item_id):
+        return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
+
+class DeepModel(Sequential):
+
+    def __init__(self, n_users, m_items, k_factors, p_dropout=0.1, **kwargs):
+        P = Sequential()
+        P.add(Embedding(n_users, k_factors, input_length=1))
+        P.add(Reshape((k_factors,)))
+        Q = Sequential()
+        Q.add(Embedding(m_items, k_factors, input_length=1))
+        Q.add(Reshape((k_factors,)))
+        super(DeepModel, self).__init__(**kwargs)
+        self.add(Merge([P, Q], mode='concat'))
+        self.add(Dropout(p_dropout))
+        self.add(Dense(k_factors, activation='relu'))
+        self.add(Dropout(p_dropout))
+        self.add(Dense(1, activation='linear'))
 
     def rate(self, user_id, item_id):
         return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
